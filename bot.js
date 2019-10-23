9712cb92ac5d6ed125f17126d32d4cbfb220d5be
Zack <zackary.divine@gmail.com>
2019-10-23 14:17:41 -0500

    Register Function

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const CryptoJs = require('crypto-js');
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 const client = new Discord.Client();
-const {PGClient} = require('pg');
+const PGClient = require('pg');
 const pgClient = new PGClient({
     host:  process.env.DATABASE_HOST,
     username: process.env.DATABASE_USER,
